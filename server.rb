7d045c01d6cd8a5a9e9d531da7747e372ab63cea
mrjoshdev <wyman.josh@gmail.com>
2018-11-27 14:23:29 -0500

    Publishing project to Github

---
+++ server.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+require 'sinatra/flash'
+require "pry" if development? || test?
+require_relative 'config/application'
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+
+configure do
+  set :views, 'app/views'
+end
+
+enable :sessions
+
+Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
+  require file
+  also_reload file
+end
+
+get '/' do
+  redirect '/parties'
+end
+
+get '/parties' do
+  erb :'parties/index'
+end
